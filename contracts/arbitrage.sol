079ea98c58cb9a66983723d0fcc405bff81aee53
Marcio Bernardes <marciobernardes@live.com>
2024-02-29 11:03:08 -0300

    add pancakeswap call function

--- contracts/arbitrage.sol
+++ contracts/arbitrage.sol
@@ -12,14 +12,13 @@ import "./interfaces/IERC20.sol";
 
 contract ArbitrageBNB is FlashLoanReceiverBase {
 
-    address public constant BNB_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public constant pancakeFactory;
     address public defi;
     IUniswapV2Router02 public bakeryRouter;
 
-    constructor(ILendingPoolAddressesProvider _addressesProvider, address _defi)
-        public
-        FlashLoanReceiverBase(_addressesProvider)
-    {
+    constructor(address _pancakeFactory, address _bakeryRouter) public {
+        pancakeFactory = _pancakeFactory;
+        bakeryRouter = IUniswapV2Router02(_bakeryRouter)
         defi = _defi;
     }
 
@@ -50,14 +49,60 @@ contract ArbitrageBNB is FlashLoanReceiverBase {
         uint256 totalDebt = _amount.add(_fee);
         transferFundsBackToPoolInternal(_reserve, totalDebt);
     }
+    
     function startArbitrage(
         address token0,
         address token1,
         uint amount0,
         uint amount1
     ) external {
-        address pairAddress = IUni
+        address pairAddress = IUniswapV2Factory(pancakeFactory).getPair(token0, token1)
+        require(pairAddress != address(0), 'This pool does not exist')
+        IUniswapV2Pair(pairAddress).swap(
+            amount0,
+            amount1,
+            address(this),
+            bytes('not empty')
+        );
     }
+
+    function pancakeCall(
+        address _sender,
+        uint _amount0,
+        uint _amount1,
+        bytes calldata _data
+    ) external {
+        address[] memory path = new address[](2)
+        uint amountToken = _amount0 == 0 ? _amount1 : _amount0;
+
+        address token0 = IUniswapV2Pair(msg.sender).token0()
+        address token1 = IUniswapV2Pair(msg.sender).token1() 
+
+        require(msg.sender == UniswapV2Library.pairFor(pancakeFactory, token0, token1), 'Unauthorized');
+        require(_amount0 == 0 || _amount1 == 0)
+
+        IERC20 token = IERC20(_amount0 == 0 ? token1: token0);
+
+        token.approve(address(bakeryRouter), amountToken);
+        uint amountRequired = UniswapV2Library.getAmountsIn(
+            pancakeFactory,
+            amountToken,
+            path
+        )[0];
+        uint amountReceived = bakeryRouter.swapExactTokensForTokens(
+            amountToken, 
+            amountRequired,
+            path,
+            msg.sender,
+            deadline
+        )[1];
+
+        IERC20 otherToken = IERC20(_amount0 == 0 ? token0 : token1)
+        otherToken.transfer(msg.sender, amountRequired);
+        otherToken.transfer(tx.origin, amountReceived - amountRequired)
+
+    }
+
     function flashloanBnb(uint256 _amount) public  {
         bytes memory data = "";
        
